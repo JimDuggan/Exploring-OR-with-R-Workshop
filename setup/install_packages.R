6b2a07cdb8b8f52220e8ddc53dff91d222f6b34e
Jim Duggan <jim.duggan@nuigalway.ie>
2023-08-25 12:39:53 +0100

    Update install_packages.R

--- setup/install_packages.R
+++ setup/install_packages.R
@@ -3,4 +3,11 @@ install.packages("ggplot2")
 install.packages("dplyr")
 install.packages("tidyr")
 install.packages("deSolve")
+install.packages("MASS")
+install.packages("GGally")
+install.packages("lpSolve")
+install.packages("igraph")
+install.packages("furrr")
+install.packages("future")
+
 
